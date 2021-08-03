858e06d81f7a1666e3c282f5dd329e254b78271a
takashi <mr.takamuu@gmail.com>
2021-08-03 13:27:26 +0900

    #2-3-1 エントリーポイント

---
+++ index.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/first */
+/* eslint-disable no-console */
+
+// console.log('index.js: loaded');
+import { App } from './src/App.js';
+
+const app = new App();
---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable import/prefer-default-export */
+
+console.log('App.js: loaded');
+export class App {
+  constructor() {
+    console.log('App initialized');
+  }
+}
